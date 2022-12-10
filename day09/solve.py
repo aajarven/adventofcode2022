b4016c303e4b6d7e2aa74804967e6a74dd68f004
Anni Järvenpää <anni.jarvenpaa@gmail.com>
2022-12-10 09:21:28 +0000

    Day 09 part 2

--- day09/solve.py
+++ day09/solve.py
@@ -5,7 +5,7 @@ Advent of code soving CLI
 import click
 
 from src import read_input
-from src.rope import count_tail_positions
+from src.rope import count_tail_positions, count_tail_positions_long
 
 
 @click.command()
@@ -16,7 +16,7 @@ def solve(input_file):
     """
     input_data = read_input.to_str_lines(input_file)
     click.echo(f"part 1: {count_tail_positions(input_data)}")
-    click.echo(f"part 2: {input_data[-1]}")
+    click.echo(f"part 2: {count_tail_positions_long(input_data)}")
 
 
 if __name__ == "__main__":
--- day09/src/rope.py
+++ day09/src/rope.py
@@ -58,3 +58,66 @@ def count_tail_positions(input_lines):
         tail = move_tail(tail, head, tail_positions)
 
     return len(tail_positions)
+
+
+def parse_head_movements(input_lines):
+    """
+    Return a list of (dx, dy) pairs that represent movements of the rope.
+    """
+    movements = []
+    for line in input_lines:
+        numbers = re.findall(r"\d+", line)
+        movement_length = int(numbers[0])
+        if "R" in line:
+            for _ in range(movement_length):
+                movements.append((1, 0))
+        if "L" in line:
+            for _ in range(movement_length):
+                movements.append((-1, 0))
+        if "U" in line:
+            for _ in range(movement_length):
+                movements.append((0, 1))
+        if "D" in line:
+            for _ in range(movement_length):
+                movements.append((0, -1))
+    return movements
+
+
+def move_head(rope, movement):
+    """
+    Move the head of the rope
+    """
+    rope[0][0] += movement[0]
+    rope[0][1] += movement[1]
+
+
+def move_rope(rope, movement):
+    """
+    Move the first knot in the rop according to the movement and the rest accordingly.
+    """
+    move_head(rope, movement)
+    for i in range(1, len(rope)):
+        knot = rope[i]
+        previous = rope[i - 1]
+        if not touching(knot, previous):
+            if knot[0] < previous[0]:
+                knot[0] += 1
+            elif knot[0] > previous[0]:
+                knot[0] -= 1
+            if knot[1] < previous[1]:
+                knot[1] += 1
+            elif knot[1] > previous[1]:
+                knot[1] -= 1
+
+
+def count_tail_positions_long(input_lines):
+    """
+    Count tail positions for a long rope with 10 knots
+    """
+    tail_positions = set()
+    head_movements = parse_head_movements(input_lines)
+    rope = [[0, 0] for _ in range(10)]
+    for movement in head_movements:
+        move_rope(rope, movement)
+        tail_positions.add(tuple(rope[-1]))
+    return len(tail_positions)
