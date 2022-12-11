361b6740026536beb94b9f79f4d6c7340787e10c
Anni Järvenpää <anni.jarvenpaa@gmail.com>
2022-12-11 12:40:21 +0000

    Add function for reading lines paragraph by paragraph

--- template/src/read_input.py
+++ template/src/read_input.py
@@ -26,3 +26,28 @@ def to_int_lines(input_file):
     Each line in file is one element in the list.
     """
     return [int(line) for line in input_file.readlines()]
+
+
+def per_paragraph_lines(input_file):
+    """
+    Return input file content as list of paragraphs.
+
+    Each paragraph is a list of lines (as strings) that make up the paragraph.
+    Paragraphs are identified as line(s) of text that are separated by an empty line.
+
+    NB: unlike in the per-line input parsing functions, newlines have been stripped from
+    the lines
+    """
+    paragraphs = []
+    current_paragraph = []
+    for line in input_file:
+        if not line.strip():
+            paragraphs.append(current_paragraph)
+            current_paragraph = []
+        else:
+            current_paragraph.append(line.strip())
+
+    if current_paragraph:
+        paragraphs.append(current_paragraph)
+
+    return paragraphs
